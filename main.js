245b607d66a48534ec0e84c2a781d64b482343b9
Tony Erskine <tony@sharptop.co>
2017-01-31 14:43:15 -0500

    added main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+const APPLICANT_NAME = "applicantName";
+
+function isExpired(obj) {
+    const ONE_HOUR = 3600000;
+    now = new Date().getTime().toString();
+    return ((now - obj.timestamp) < ONE_HOUR);
+}
+
+function getApplicantNameFromStorage() {
+    var object = JSON.parse(localStorage.getItem(APPLICANT_NAME));
+
+    if (object  && isExpired(object)) {
+        localStorage.removeItem(APPLICANT_NAME);
+        object = null
+    }
+
+    var returnVal = (object ? object.value : "");
+    console.log("returnVal === " + returnVal);
+    return returnVal;
+}
+
+function updateApplicantName(nameField) {
+    var applicantName = {value: nameField.val(), timestamp: new Date().getTime()}
+    localStorage.setItem(APPLICANT_NAME, JSON.stringify(applicantName));
+}
